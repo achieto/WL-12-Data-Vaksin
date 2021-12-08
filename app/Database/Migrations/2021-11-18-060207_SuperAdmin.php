2786c3fc3c3bd6c0c724adb005703cb7b3684a1d
apriladwiutami <apriladwi68@gmail.com>
2021-12-09 00:22:09 +0700

    Aprila

--- app/Database/Migrations/2021-11-18-060207_SuperAdmin.php
+++ app/Database/Migrations/2021-11-18-060207_SuperAdmin.php
@@ -6,13 +6,36 @@ use CodeIgniter\Database\Migration;
 
 class SuperAdmin extends Migration
 {
+	
 	public function up()
 	{
-		
+		$this->forge->addField([
+			'id'				=> [
+				'type'           => 'INT',
+				'constraint'     => 11,
+				'auto_increment' => true,
+			],
+			'username'          => [
+				'type'           => 'VARCHAR',
+				'constraint'     => '30',
+			],
+			'password'       => [
+				'type'           => 'VARCHAR',
+				'constraint'     => '8',
+			],
+			'nama'       => [
+				'type'           => 'VARCHAR',
+				'constraint'     => '35',
+			],
+			
+			
+		]);
+		$this->forge->addKey('id', true);
+		$this->forge->createTable('loginsuper');
 	}
 
 	public function down()
 	{
-
+		$this->forge->dropTable('loginsuper');
 	}
 }
