db069baca7be5deaf0f6de06cedf449463bb1271
AnisaNadila26 <anisanadila26010.anl@gmail.com>
2021-12-09 01:22:29 +0700

    Lase

--- app/Models/AdminModel.php
+++ app/Models/AdminModel.php
@@ -6,13 +6,8 @@ class AdminModel extends Model
     public function get_data($username, $password)
     {
         return $this->db->table('login')->where(array('username' => $username, 'password' => $password))->get()->getRowArray();
-        // $builder = $this->db->table($tbl);
-        // $builder->where('username', $username);
-        // $log = $builder->get()->getRow;
-        // return $log;
     }
     protected $table                = 'login';
     protected $primaryKey           = 'username';
     protected $allowedFields        = ['username', 'password', 'nama'];
-    // protected $useTimestamp            = true;
 }
--- app/Models/SuperModel.php
+++ app/Models/SuperModel.php
@@ -6,13 +6,8 @@ class SuperModel extends Model
     public function get_data($username, $password)
     {
         return $this->db->table('loginsuper')->where(array('username' => $username, 'password' => $password))->get()->getRowArray();
-        // $builder = $this->db->table($tbl);
-        // $builder->where('username', $username);
-        // $log = $builder->get()->getRow;
-        // return $log;
     }
     protected $table                = 'loginsuper';
     protected $primaryKey           = 'username';
     protected $allowedFields        = ['username', 'password', 'nama'];
-    // protected $useTimestamp            = true;
 }
